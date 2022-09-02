d99d9c8957dc4436dc1fb0a61735b6116257293c
andreluiz19 <101955093+andreluiz19@users.noreply.github.com>
2022-09-02 08:57:21 -0300

    refatoracao

--- src/arquitetura_de_software/exercicios/ToDo.java
+++ src/arquitetura_de_software/exercicios/ToDo.java
@@ -9,44 +9,29 @@ public class ToDo {
 		
 		Scanner scanner = new Scanner(System.in);
 		ArrayList<String> tasks = new ArrayList<String>();
-		boolean next = false;
-		int quest = 1;
+		int resp = 1;
 		int remove;
 		
-		do {
-			System.out.print("Digite uma tarefa para inserir em sua lista de tarefas: ");
-			tasks.add(scanner.nextLine());
-			System.out.print("Deseja inserir uma nova tarefa [1]Sim [2]Não: ");
-			quest = scanner.nextInt();
-			scanner.nextLine(); //Limpando lixo de memória
-			next = quest == 1 ? false : true;
-		} while(!next);
-		
-		getTasks(tasks);
-		next = false;
-		quest = 1;
-		
-		do {
-			System.out.print("\nDeseja retirar uma tarefa já concluída? [1]Sim [2]Não: ");
-			quest = scanner.nextInt();
-			scanner.nextLine(); //Limpando lixo de memória
-			if(quest == 1) {
+		while(resp != 0) {
+			System.out.println("\nLista de Tarefas\n");
+			for(int i = 0; i< tasks.size(); i++) {
+				System.out.println("ID " + i + " " + "-> " + tasks.get(i));
+			}
+			System.out.print("\nSelecione uma opção [1]Adicionar - [2]Excluir - [3]Fechar -> ");
+			resp = scanner.nextInt();
+			scanner.nextLine();
+			if(resp == 1) {
+				System.out.print("Digite uma tarefa para inserir em sua lista de tarefas: ");
+				tasks.add(scanner.nextLine());
+			}else if(resp == 2) {
 				System.out.print("Insira o ID da tarefa para retirá-la da lista: ");
 				remove = scanner.nextInt();
 				scanner.nextLine();
 				tasks.remove(remove);
+			}else {
+				resp = 0;
 			}
-			next = quest == 1 ? false : true;
-		} while(!next);
-	
-		getTasks(tasks);
-		scanner.close();
-	}
-	
-	public static void getTasks(ArrayList<String> tasks) {
-		System.out.println("\nLista de Tarefas\n");
-		for(int i = 0; i< tasks.size(); i++) {
-			System.out.println("ID " + i + " " + "-> " + tasks.get(i));
 		}
+		scanner.close();
 	}
 }
